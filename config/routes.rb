4c02e4538754a1e2094206e49e433ba7e738fbe9
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-28 19:47:07 +0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/signup',to:'users#new'
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
   resources :users
 end
