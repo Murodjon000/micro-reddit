23aac3c4ac1ec8d2b1bffe4f720b0135a5f521e8
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-28 10:48:59 +0500

    added application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title=' ')
+        base_titel='Reddit app'
+        if page_title.empty?
+           base_titel
+        else
+            page_title + ' | '+ base_titel
+        end
+    end
 end
